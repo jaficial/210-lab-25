b9ea725789015da011db44a72477b0a2896d219e
Jed Aficial's PC <jedaficial1@gmail.com>
2024-10-27 21:28:58 -0700

    10-minute update

--- main.cpp
+++ main.cpp
@@ -12,17 +12,6 @@ using namespace std::chrono;
 
 string TEST_CODE = "TESTCODE";
 
-// void output(string, string, string, string);
-
-// void output(string read_result = "", string sort_result = "", string insert_result = "", string delete_result = ""){
-//     cout << setw(10) << "Operation" << setw(10) << "Vector" << setw(10) << "List" << setw(10) << "Set" << endl;
-//     cout << 
-// }
-/* NOTE: - Need to read the 20,000 lines of data from codes.txt into vector, list, and set
-         - Need to sort the vector, sort the list. (Set is already sorted) 
-         - Insert the value "TESTCODE" in the middle of the vecotr, middle of the list, and 
-           middle of the set
-         - Delete the middle element of the vector, list, and set */ 
 int main() {
 
     ifstream fin_vector("codes.txt");
@@ -36,13 +25,13 @@ int main() {
     }
     fin_vector.close();
     auto end = chrono::high_resolution_clock::now();
-    auto duration_vector_read = duration_cast<microseconds>(end - start);
+    auto vector_example_read = duration_cast<microseconds>(end - start);
 
     // sorting the vector
     start = chrono::high_resolution_clock::now();
     sort(vector_example.begin(), vector_example.end()); // CITED from "Lesson Code: STD::vector" module from canvas
     end = chrono::high_resolution_clock::now();
-    auto duration_vector_sort = duration_cast<microseconds>(end - start);
+    auto vector_example_sort = duration_cast<microseconds>(end - start);
 
     // inserting "TESTCODE" into the middle of the vector
     int vector_midpoint = vector_example.size();
@@ -51,16 +40,16 @@ int main() {
     start = chrono::high_resolution_clock::now();
     vector_example.insert(vector_example.begin() + vector_midpoint, TEST_CODE);
     end = chrono::high_resolution_clock::now();
-    auto duration_vector_insert = duration_cast<microseconds>(end - start);
+    auto vector_example_insert = duration_cast<microseconds>(end - start);
 
     // deleting the middle element from the vector
 
     start = chrono::high_resolution_clock::now();
     vector_example.erase(vector_example.begin() + vector_midpoint); // CITED from https://cplusplus.com/reference/vector/vector/erase/ to learn how to use ".erase()" function
     end = chrono::high_resolution_clock::now();
-    auto duration_vector_delete = duration_cast<microseconds>(end - start);
+    auto vector_example_delete = duration_cast<microseconds>(end - start);
 
-    // ----------------------------------------------
+    // beginning collecting data for LISTS
 
     ifstream fin_list("codes.txt");
     list<string> list_example;
@@ -71,7 +60,7 @@ int main() {
         list_example.push_back(temp_string);
     }
     end = chrono::high_resolution_clock::now();
-    auto duration_list_read = duration_cast<microseconds>(end - start);
+    auto list_example_read = duration_cast<microseconds>(end - start);
     fin_list.close();
 
     // sorting the list
@@ -100,9 +89,7 @@ int main() {
     end = chrono::high_resolution_clock::now();
     auto list_example_delete = duration_cast<microseconds>(end - start);
 
-    
-    // ----------------------------------------------
-
+    // beginning collecting data for SETS
     ifstream fin_set("codes.txt");
     set<string> set_example;
     // reading the file into the set
@@ -130,16 +117,22 @@ int main() {
     set_example.insert(set_iter, TEST_CODE);
     end = chrono::high_resolution_clock::now();
     auto set_example_insert = duration_cast<microseconds>(end - start);
-    
+
     // Deleting the middle element of a set
     start = chrono::high_resolution_clock::now();
     set_example.erase(set_iter);
     end = chrono::high_resolution_clock::now();
     auto set_example_delete = duration_cast<microseconds>(end - start);
     
+    //------------------------------------------------
+    cout << setw(10) << "Operation" << setw(10) << "Vector" << setw(10) << "List" << setw(10) << "Set" << endl;
+    cout << setw(10) << "Read" << setw(10) << vector_example_read.count() << setw(10) << list_example_read.count() << setw(10) << set_example_read.count() << endl;
+    cout << setw(10) << "Sort" << setw(10) << vector_example_sort.count() << setw(10) << list_example_sort.count() << setw(10) << set_example_sort << endl;
+    cout << setw(10) << "Insert" << setw(10) << vector_example_insert.count() << setw(10) << list_example_insert.count() << setw(10) << set_example_insert.count() << endl;
+    cout << setw(10) << "Delete" << setw(10) << vector_example_delete.count() << setw(10) << list_example_delete.count() << setw(10) << set_example_delete.count() << endl;
+
     return 0;
 }
-
 /* syntax examples:
 auto start = high_resolution_clock::now()
 auto end = high_resolution_clock::now()
