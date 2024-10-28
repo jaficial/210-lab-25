1ee5dbae9a246d6db3afc735f32bcfcf806a1d2e
Jed Aficial's PC <jedaficial1@gmail.com>
2024-10-27 17:59:44 -0700

    10-minute update

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <chrono>
 #include <vector>
 #include <list>
@@ -6,8 +7,19 @@
 
 using namespace std;
 
+/* NOTE: - Need to read the 20,000 lines of data from codes.txt into vector, list, and set
+         - Need to sort the vector, sort the list. (Set is already sorted) 
+         - Insert the value "TESTCODE" in the middle of the vecotr, middle of the list, and 
+           middle of the set
+         - Delete the middle element of the vector, list, and set */ 
 int main() {
     
+    ifstream fin("codes.txt");
+    auto start = chrono::high_resolution_clock::now();
+    vector<string> vector_read; 
+    while (getline())
+    
+
 
     return 0;
 }
