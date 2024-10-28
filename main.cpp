07bbbb4d1dc960ef207a833c75da6037f94414e3
Jed Aficial's PC <jedaficial1@gmail.com>
2024-10-27 19:53:47 -0700

    10-minute update

--- main.cpp
+++ main.cpp
@@ -11,60 +11,39 @@ using namespace std;
 using namespace std::chrono;
 
 int menu();
-// CITED: Cited the "main_menu" function from my Lab 23/24 submission 
-int menu(){
-    int choice;
+void output(string, string, string, string);
 
-    cout << endl << "Select a race that you would like to perform from the list below:" << endl;
-    cout << "[1] Read" << endl;
-    cout << "[2] Sort" << endl;
-    cout << "[3] Insert" << endl;
-    cout << "[4] Delete" << endl;
-    cout << "[5] End" << endl;
-    cout << "Choice --> ";
-    cin >> choice;
-    if ((choice < 1) || (choice >> 5)){
-        cout << "Invalid Choice" << endl;
-    }
-    return choice;
+void output(string read_result = "", string sort_result = "", string insert_result = "", string delete_result = ""){
+    cout << setw(10) << "Operation" << setw(10) << "Vector" << setw(10) << "List" << setw(10) << "Set" << endl;
+    cout << 
 }
-
 /* NOTE: - Need to read the 20,000 lines of data from codes.txt into vector, list, and set
          - Need to sort the vector, sort the list. (Set is already sorted) 
          - Insert the value "TESTCODE" in the middle of the vecotr, middle of the list, and 
            middle of the set
          - Delete the middle element of the vector, list, and set */ 
 int main() {
-    bool race_flag = false;
-    while (!race_flag){
-        int menu_choice;
-        menu_choice = menu();
-
-        if (menu_choice == 5){
-            race_flag = true;
-        }
-
-        // Reading race
-        if (menu_choice == 1){
-            cout << setw(10) << "Operation" << setw(10) << "Vector" << setw(10) << "List" << setw(10) << "Set" << endl;
-            // reading for vector:
-            ifstream fin("codes.txt");
-            auto start = chrono::high_resolution_clock::now();
-            vector<string> vector_read;
-            string temp_string; 
-    
-            for (int i = 0; i < 20000; i++){
-                getline(fin, temp_string);
-                vector_read.push_back(temp_string);
-            }
-            fin.close();
-            auto end = chrono::high_resolution_clock::now();
-            auto duration_vector_read = duration_cast<microseconds>(end - start);
-            cout << setw(10) << "Read" << setw(10) << duration_vector_read.count();
-        }
-
-
+    ifstream fin_vector("codes.txt");
+    auto start = chrono::high_resolution_clock::now();
+    vector<string> vector_;
+    string temp_string; 
+    // reading for vector:
+    for (int i = 0; i < 20000; i++){
+        getline(fin_vector, temp_string);
+        vector_read.push_back(temp_string);
     }
+    fin_vector.close();
+    auto end = chrono::high_resolution_clock::now();
+    auto duration_vector_read = duration_cast<microseconds>(end - start);
+
+    start = chrono::high_resolution_clock::now();
+    // reading for list:
+    ifstream fin_list("codes.txt");
+
+
+
+
+    
 
 
     return 0;
