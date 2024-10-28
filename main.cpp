eb797c0eb42b8ea9b85380c044db1ce8f8b7ca83
Jed Aficial's PC <jedaficial1@gmail.com>
2024-10-27 20:15:31 -0700

    10-minute update

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@
 using namespace std;
 using namespace std::chrono;
 
+string TEST_CODE = "TESTCODE";
+
 // void output(string, string, string, string);
 
 // void output(string read_result = "", string sort_result = "", string insert_result = "", string delete_result = ""){
@@ -23,6 +25,7 @@ using namespace std::chrono;
            middle of the set
          - Delete the middle element of the vector, list, and set */ 
 int main() {
+
     ifstream fin_vector("codes.txt");
     auto start = chrono::high_resolution_clock::now();
     vector<string> vector_example;
@@ -41,10 +44,17 @@ int main() {
     sort(vector_example.begin(), vector_example.end()); // CITED from "Lesson Code: STD::vector" module from canvas
     end = chrono::high_resolution_clock::now();
     auto duration_vector_sort = duration_cast<microseconds>(end - start);
-    
-    cout << "This is the vector_read: " << duration_vector_read.count() << endl << "This is the vector_sort" << duration_vector_sort.count() << endl;
-    // inserting "TESTCODE" into the middle of the vector
 
+    // inserting "TESTCODE" into the middle of the vector
+    int vector_midpoint = vector_example.size();
+    vector_midpoint = (vector_midpoint / 2) - 1; // -1 since range is between 0-19999
+    
+    start = chrono::high_resolution_clock::now();
+    vector_example.insert(vector_example.begin() + vector_midpoint, TEST_CODE);
+    end = chrono::high_resolution_clock::now();
+    auto duration_vector_insert = duration_cast<microseconds>(end - start);
+
+    //
 
     // reading for list:
     // ifstream fin_list("codes.txt");
